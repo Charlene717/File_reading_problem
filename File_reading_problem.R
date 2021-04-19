37b039a9059bc0ccbad769b69771cc7811d15b08
Ch_HCC <q2330416@gmail.com>
2021-04-19 12:57:47 +0800

    Ori

---
+++ File_reading_problem.R
@@ -0,0 +1,40 @@
+rm(list = ls()) #清除變數
+
+# Main file
+setwd(getwd()) ## Set current working directory
+PathName <- getwd() ## Set output directroy
+#
+# datatext <- read.table(paste0(PathName,"/mouse_compartment_textmining_full.tsv"),  # 資料檔名
+#                       header=F,          # 資料中的第一列，作為欄位名稱
+#                       sep="\t")           # 將逗號視為分隔符號來讀取資料
+
+datainte <- read.table(paste0(PathName,"/mouse_compartment_integrated_full.tsv"),  # 資料檔名 
+                      header=F,          # 資料中的第一列，作為欄位名稱
+                      sep="\t")           # 將逗號視為分隔符號來讀取資料
+# dataknow <- read.table(paste0(PathName,"/mouse_compartment_knowledge_full.tsv"),  # 資料檔名
+#                        header=F,          # 資料中的第一列，作為欄位名稱
+#                        sep="\t")           # 將逗號視為分隔符號來讀取資料
+
+# Test csv
+subcellular <- read.csv(paste0(PathName,"/subcellular2.csv"),header = F)  # 資料檔名
+    #                   header=F,          # 資料中的第一列，作為欄位名稱
+     #                  sep=",")           # 將逗號視為分隔符號來讀取資料
+subcellular2 <- read.table(paste0(PathName,"/subcellular2.csv"),  # 資料檔名
+                           header=F,          # 資料中的第一列，作為欄位名稱
+                           sep=",")           # 將逗號視為分隔符號來讀取資料
+subcellular3 <- read.delim(paste0(PathName,"/subcellular2.csv"),  # 資料檔名
+                       header=F,          # 資料中的第一列，作為欄位名稱
+                       sep=",")           # 將逗號視為分隔符號來讀取資料
+# Test
+subcellular_E1 <- read.table(paste0(PathName,"/subcellular.xltx"),  # 資料檔名
+                       header=F,          # 資料中的第一列，作為欄位名稱
+                       sep=" ")           # 將逗號視為分隔符號來讀取資料
+
+
+### !!! https://stackoverflow.com/questions/12828438/read-csv-vs-read-table
+datainte2 <- read.delim(paste0(PathName,"/mouse_compartment_integrated_full.tsv"),  # 資料檔名 
+                       header=F,          # 資料中的第一列，作為欄位名稱
+                       sep="\t")           # 將逗號視為分隔符號來讀取資料
+
+
+
